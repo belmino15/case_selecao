03be90a01aa11649240282a9c473b082a3b42058
Lucas Belmino <lucas.belmino@hotmail.com>
2020-08-09 17:41:39 -0300

    Minor fix in all answers

--- questao2/query4.sql
+++ questao2/query4.sql
@@ -1,13 +1,6 @@
 use db_fort;
 
-select max(dt_ref), p.ID_PESSOA from f_vendas v
+select p.ID_PESSOA, max(dt_ref) from f_vendas v
 left join d_pessoa p on p.ID_PESSOA = v.ID_PESSOA
 left join d_tempo t on t.ID_TEMPO = v.ID_TEMPO
 group by p.id_pessoa;
-
-/*
-select * from f_vendas v
-left join d_pessoa p on p.ID_PESSOA = v.ID_PESSOA
-left join d_tempo t on t.ID_TEMPO = v.ID_TEMPO
-order by p.ID_PESSOA;
-*/
